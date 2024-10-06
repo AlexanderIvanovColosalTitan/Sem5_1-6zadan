16d3939012fc5e544bf7e59dac1afdfef71e93c5
AlexanderIvanovColosalTitan <iwanov.sasha13@yandex.ru>
2024-10-06 12:31:54 +0300

    .

--- Zadan1/Main.java
+++ Zadan1/Main.java
@@ -1,40 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.*;
+import java.awt.*;
 
 class Composition {
-    private List<Instrument> instruments;
+    private final List<Instrument> instruments = new ArrayList<>();
     private String name;
 
     public Composition(String name) {
-        this.instruments = new ArrayList<>();
         this.name = name;
     }
 
-    public void addInstrument(Instrument instrument) {
-        instruments.add(instrument);
-    }
-
-    public void removeInstrument(Instrument instrument) {
-        instruments.remove(instrument);
-    }
-
-    public List<Instrument> getInstruments() {
-        return instruments;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void addInstrument(Instrument instrument) { instruments.add(instrument); }
+    public void removeInstrument(Instrument instrument) { instruments.remove(instrument); }
+    public List<Instrument> getInstruments() { return instruments; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
 
 abstract class Instrument {
     protected String name;
-    protected int volume;
-    protected int tempo;
+    protected int volume, tempo;
 
     public Instrument(String name, int volume, int tempo) {
         this.name = name;
@@ -42,38 +28,14 @@ abstract class Instrument {
         this.tempo = tempo;
     }
 
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public int getTempo() {
-        return tempo;
-    }
-
-    public void setTempo(int tempo) {
-        this.tempo = tempo;
-    }
+    public int getVolume() { return volume; }
+    public void setVolume(int volume) { this.volume = volume; }
+    public int getTempo() { return tempo; }
+    public void setTempo(int tempo) { this.tempo = tempo; }
 }
 
-class Guitar extends Instrument {
-    public Guitar() {
-        super("Гитара", 50, 120);
-    }
-}
+class Guitar extends Instrument { public Guitar() { super("Гитара", 50, 120); } }
+class Fortepiano extends Instrument { public Fortepiano() { super("Фортепиано", 50, 120); } }
+class Skripka extends Instrument { public Skripka() { super("Скрипка", 50, 120); } }
 
-class Fortepiano extends Instrument {
-    public Fortepiano() {
-        super("Фортепиано", 50, 120);
-    }
-}
-
-class Skripka extends Instrument {
-    public Skripka() {
-        super("Скрипка", 50, 120);
-    }
-}
 
--- Zadan1/MainFrame.java
+++ Zadan1/MainFrame.java
@@ -1,98 +1,93 @@
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame extends JFrame {
-    private JList<Composition> compositionList;
-    private DefaultListModel<Composition> compositionListModel;
-    private JTextField compositionNameField;
-    private JList<Instrument> instrumentList;
-    private DefaultListModel<Instrument> instrumentListModel;
-    private JSpinner volumeSpinner;
-    private JSpinner tempoSpinner;
+    private final DefaultListModel<Composition> compositionListModel = new DefaultListModel<>();
+    private final DefaultListModel<Instrument> instrumentListModel = new DefaultListModel<>();
+    private final JList<Composition> compositionList = new JList<>(compositionListModel);
+    private final JList<Instrument> instrumentList = new JList<>(instrumentListModel);
+    private final JTextField compositionNameField = new JTextField();
+    private final JSpinner volumeSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+    private final JSpinner tempoSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 300, 1));
 
     public MainFrame() {
         setTitle("Музыкальное приложение");
         setSize(800, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        compositionListModel = new DefaultListModel<>();
-        compositionList = new JList<>(compositionListModel);
         compositionList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        compositionList.addListSelectionListener(e -> {
-            Composition composition = compositionList.getSelectedValue();
-            if (composition != null) {
-                instrumentListModel.clear();
-                for (Instrument instrument : composition.getInstruments()) {
-                    instrumentListModel.addElement(instrument);
-                }
-                compositionNameField.setText(composition.getName());
-            }
-        });
+        compositionList.addListSelectionListener(e -> updateInstrumentList());
 
-        JButton newCompositionButton = new JButton("Создать новую композицию");
-        newCompositionButton.addActionListener(e -> createNewComposition());
-
-        JButton deleteCompositionButton = new JButton("Удалить композицию");
-        deleteCompositionButton.addActionListener(e -> deleteSelectedComposition());
-
-        instrumentListModel = new DefaultListModel<>();
-        instrumentList = new JList<>(instrumentListModel);
         instrumentList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        instrumentList.addListSelectionListener(e -> {
-            Instrument instrument = instrumentList.getSelectedValue();
-            if (instrument != null) {
-                volumeSpinner.setValue(instrument.getVolume());
-                tempoSpinner.setValue(instrument.getTempo());
-            }
-        });
+        instrumentList.addListSelectionListener(e -> updateSpinnerValues());
 
-        JButton addInstrumentButton = new JButton("Добавить инструмент");
-        addInstrumentButton.addActionListener(e -> addNewInstrument());
-
-        JButton deleteInstrumentButton = new JButton("Удалить инструмент");
-        deleteInstrumentButton.addActionListener(e -> deleteSelectedInstrument());
-
-        compositionNameField = new JTextField();
-        compositionNameField.addActionListener(e -> updateSelectedComposition());
-
-        volumeSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
-        volumeSpinner.addChangeListener(e -> updateSelectedInstrument());
-
-        tempoSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 300, 1));
-        tempoSpinner.addChangeListener(e -> updateSelectedInstrument());
-
-        JPanel compositionPanel = new JPanel(new BorderLayout());
-        compositionPanel.setBorder(BorderFactory.createTitledBorder("Композиции"));
-        compositionPanel.add(new JScrollPane(compositionList), BorderLayout.CENTER);
-
-        JPanel compositionButtonPanel = new JPanel(new GridLayout(1, 2));
-        compositionButtonPanel.add(newCompositionButton);
-        compositionButtonPanel.add(deleteCompositionButton);
-        compositionPanel.add(compositionButtonPanel, BorderLayout.SOUTH);
-
-        JPanel instrumentPanel = new JPanel(new BorderLayout());
-        instrumentPanel.setBorder(BorderFactory.createTitledBorder("Инструменты"));
-        instrumentPanel.add(new JScrollPane(instrumentList), BorderLayout.CENTER);
-
-        JPanel instrumentButtonPanel = new JPanel(new GridLayout(1, 2));
-        instrumentButtonPanel.add(addInstrumentButton);
-        instrumentButtonPanel.add(deleteInstrumentButton);
-        instrumentPanel.add(instrumentButtonPanel, BorderLayout.SOUTH);
-
-        JPanel editPanel = new JPanel(new GridLayout(3, 2));
-        editPanel.setBorder(BorderFactory.createTitledBorder("Редактирование"));
-        editPanel.add(new JLabel("Громкость:"));
-        editPanel.add(volumeSpinner);
-        editPanel.add(new JLabel("Темп:"));
-        editPanel.add(tempoSpinner);
-
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.add(compositionPanel, BorderLayout.WEST);
-        mainPanel.add(instrumentPanel, BorderLayout.CENTER);
-        mainPanel.add(editPanel, BorderLayout.EAST);
-
-        add(mainPanel);
+        createUI();
         setVisible(true);
+    }
+
+    private void createUI() {
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(createCompositionPanel(), BorderLayout.WEST);
+        mainPanel.add(createInstrumentPanel(), BorderLayout.CENTER);
+        mainPanel.add(createEditPanel(), BorderLayout.EAST);
+        add(mainPanel);
+    }
+
+    private JPanel createCompositionPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createTitledBorder("Композиции"));
+        panel.add(new JScrollPane(compositionList), BorderLayout.CENTER);
+        JButton newButton = new JButton("Создать новую композицию");
+        newButton.addActionListener(e -> createNewComposition());
+        JButton deleteButton = new JButton("Удалить композицию");
+        deleteButton.addActionListener(e -> deleteSelectedComposition());
+        panel.add(new JPanel(new GridLayout(1, 2)) {{
+            add(newButton); add(deleteButton);
+        }}, BorderLayout.SOUTH);
+        return panel;
+    }
+
+    private JPanel createInstrumentPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createTitledBorder("Инструменты"));
+        panel.add(new JScrollPane(instrumentList), BorderLayout.CENTER);
+        JButton addButton = new JButton("Добавить инструмент");
+        addButton.addActionListener(e -> addNewInstrument());
+        JButton deleteButton = new JButton("Удалить инструмент");
+        deleteButton.addActionListener(e -> deleteSelectedInstrument());
+        panel.add(new JPanel(new GridLayout(1, 2)) {{
+            add(addButton); add(deleteButton);
+        }}, BorderLayout.SOUTH);
+        return panel;
+    }
+
+    private JPanel createEditPanel() {
+        JPanel panel = new JPanel(new GridLayout(3, 2));
+        panel.setBorder(BorderFactory.createTitledBorder("Редактирование"));
+        panel.add(new JLabel("Громкость:")); panel.add(volumeSpinner);
+        panel.add(new JLabel("Темп:")); panel.add(tempoSpinner);
+        return panel;
+    }
+
+    private void updateInstrumentList() {
+        Composition composition = compositionList.getSelectedValue();
+        if (composition != null) {
+            instrumentListModel.clear();
+            for (Instrument instrument : composition.getInstruments()) {
+                instrumentListModel.addElement(instrument);
+            }
+            compositionNameField.setText(composition.getName());
+        }
+    }
+
+    private void updateSpinnerValues() {
+        Instrument instrument = instrumentList.getSelectedValue();
+        if (instrument != null) {
+            volumeSpinner.setValue(instrument.getVolume());
+            tempoSpinner.setValue(instrument.getTempo());
+        }
     }
 
     private void createNewComposition() {
@@ -114,23 +109,15 @@ public class MainFrame extends JFrame {
     private void addNewInstrument() {
         Composition composition = compositionList.getSelectedValue();
         if (composition != null) {
-            String[] options = {"Гитара", "Фортепиано", "Скрипка"};
-            String instrumentType = (String) JOptionPane.showInputDialog(this, "Выберите инструмент:",
-                    "Добавить инструмент", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
-            Instrument instrument = null;
-
+            String instrumentType = (String) JOptionPane.showInputDialog(this, "Выберите инструмент:", "Добавить инструмент",
+                    JOptionPane.QUESTION_MESSAGE, null, new String[]{"Гитара", "Фортепиано", "Скрипка"}, "Гитара");
             if (instrumentType != null) {
-                switch (instrumentType) {
-                    case "Гитара":
-                        instrument = new Guitar();
-                        break;
-                    case "Фортепиано":
-                        instrument = new Fortepiano();
-                        break;
-                    case "Скрипка":
-                        instrument = new Skripka();
-                        break;
-                }
+                Instrument instrument = switch (instrumentType) {
+                    case "Гитара" -> new Guitar();
+                    case "Фортепиано" -> new Fortepiano();
+                    case "Скрипка" -> new Skripka();
+                    default -> null;
+                };
                 if (instrument != null) {
                     composition.addInstrument(instrument);
                     instrumentListModel.addElement(instrument);
@@ -149,26 +136,7 @@ public class MainFrame extends JFrame {
         }
     }
 
-    private void updateSelectedComposition() {
-        Composition composition = compositionList.getSelectedValue();
-        if (composition != null) {
-            composition.setName(compositionNameField.getText());
-            compositionList.repaint();
-        }
-    }
-
-    private void updateSelectedInstrument() {
-        Composition composition = compositionList.getSelectedValue();
-        Instrument instrument = instrumentList.getSelectedValue();
-        if (composition != null && instrument != null) {
-            instrument.setVolume((int) volumeSpinner.getValue());
-            instrument.setTempo((int) tempoSpinner.getValue());
-            instrumentList.repaint();
-        }
-    }
-
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MainFrame());
+        SwingUtilities.invokeLater(MainFrame::new);
     }
 }
-
